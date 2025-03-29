74b0dc2bc77b4d6b85880509ab325f76347f97d1
aaleejandro01 <alejandromorilla2001@gmail.com>
2025-03-29 12:45:36 +0100

    l05 finished

--- alejandro/index.js
+++ alejandro/index.js
@@ -1,16 +1,34 @@
 const express = require("express");
-const cool = require("cool-ascii-faces")
 const app = express();
 const PORT = process.env.PORT || 16078;
+const BASE_API = "/api/v1";
+app.use(express.json());
 
 app.use("/",express.static("./public"));
 
-app.get("/hello",(request,response) => {
-    response.send("Hello from the server!");
+let contacts = [
+    {
+        name: "peter",
+        phone: 123456
+    },
+    {
+        name: "pablo",
+        phone: 789042
+    }
+];
+
+app.get(BASE_API + "/contacts",(request,response) => {
+    console.log("New GET to /contacts");
+    response.send(JSON.stringify(contacts));
 });
 
-app.get("/cool",(request,response) => {
-    response.send(cool());
+app.post(BASE_API + "/contacts",(request,response) => {
+    console.log("POST to /contacts");
+    let newContact = request.body;
+    
+    contacts.push(newContact);
+
+    response.sendStatus(201);
 });
 
 app.listen(PORT,()=>{
